0c0e8a2e3b48e9d8dea7752be776649c616b52b4
Fadhil Yaacob <piukul@yahoo.com.my>
2022-08-27 21:50:23 +0800

    fix POST redirection logic

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,29 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { Client } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+
+// const client = new Client({
+//   user: "postgres",
+//   password: "mysecretpassword",
+//   database: "postgres",
+//   hostname: "localhost",
+//   port: 5432,
+// });
+// await client.connect();
 
 export const handler: Handlers<null> = {
-  POST(_, ctx): Response {
-      return Response.redirect('http://localhost:8000/details/x')
+  async POST(_, ctx): Promise<Response> {
+    // const result = await client.queryArray(`SELECT 'Hello'`)
+    // console.log(result.rows[0][0])
+
+    // How to handle relative path redirection
+    // https://github.com/denoland/fresh/discussions/511#discussioncomment-3157429
+    return new Response("", {
+      status: 302,
+      headers: { Location: "/details/x" },
+    });
   },
 };
 
@@ -19,8 +38,8 @@ export default function Home() {
           <label>
             Long URL
           </label>
-          <input type='text'/>
-          <input type='submit'/>
+          <input type='text' />
+          <input type='submit' />
         </form>
       </p>
     </div>
