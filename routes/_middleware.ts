960c0b9d5c29c9efa680c264fc27e43998ba6264
Fadhil Yaacob <piukul@yahoo.com.my>
2022-08-29 20:54:17 +0800

    fix auth middleware

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,5 +1,5 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
-import { getCookies } from "$std/http/cookie.ts";
+import { getCookies, deleteCookie } from "$std/http/cookie.ts";
 import * as postgres from "https://deno.land/x/postgres@v0.16.1/mod.ts";
 
 // Connect to Postgres
@@ -12,37 +12,35 @@ const pool = new postgres.Pool(databaseUrl, 3, true);
 const connection = await pool.connect();
 
 interface State {
-  loggedIn: boolean;
-  username: String;
+    loggedIn: boolean;
+    username: String;
 }
 
 export async function handler(
-  req: Request,
-  ctx: MiddlewareHandlerContext<State>,
+    req: Request,
+    ctx: MiddlewareHandlerContext<State>,
 ) {
-  const accessToken = getCookies(req.headers)["session_token"];
+    const accessToken = getCookies(req.headers)["session_token"];
 
-  if (accessToken) {
-    const result = await connection.queryObject(
-      "SELECT username FROM users WHERE access_token = $TOKEN AND access_token IS NOT NULL",
-      { token: accessToken },
-    );
+    if (accessToken) {
+        const result = await connection.queryObject(
+            "SELECT username FROM users WHERE access_token = $TOKEN AND access_token IS NOT NULL",
+            { token: accessToken },
+        );
 
-    if (result.rowCount > 0) {
-      ctx.state.loggedIn = true;
-      ctx.state.username = result.rows[0].username;
-      const resp = await ctx.next();
-      return resp;
-    } else {
-      return new Response("", {
-        status: 302,
-        headers: { location: "/api/logout" },
-      });
+        if (result.rowCount > 0) {
+            ctx.state.loggedIn = true;
+            ctx.state.username = result.rows[0].username;
+            const resp = await ctx.next();
+            return resp;
+        } else {
+            const headers = new Headers({});
+            deleteCookie(headers, "session_token", { path: "/" });
+        }
     }
-  }
 
-  // If no access token or invalid access_token
-  ctx.state.loggedIn = false;
-  const resp = await ctx.next();
-  return resp;
+    // If no access token or invalid access_token
+    ctx.state.loggedIn = false;
+    const resp = await ctx.next();
+    return resp;
 }
